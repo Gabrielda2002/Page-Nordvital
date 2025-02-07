aa55b3677640f99e32e8699664e2786e5049c721
Gabriel Duarte <duartearias2002@gmail.com>
2025-02-07 14:20:03 -0500

    Add: tap servicio laboratorio

---
+++ src/content/services-md-esp.ts
@@ -0,0 +1,120 @@
+import medFaIcon from '@/assets/services_icons/medEsp/servicio-1.svg'
+import medInIcon from '@/assets/services_icons/medEsp/servicio-2.svg'
+import pediatriaIcon from '@/assets/services_icons/medEsp/servicio-3.svg'
+import cardiologiaIcon from '@/assets/services_icons/medEsp/servicio-4.svg'
+import ginecobsIcon from '@/assets/services_icons/medEsp/servicio-6.svg'
+import endoIcon from '@/assets/services_icons/medEsp/servicio-7.svg'
+import otoIcon from '@/assets/services_icons/medEsp/servicio-9.svg'
+import nefroIcon from '@/assets/services_icons/medEsp/servicio-10.svg'
+import neuIcon from '@/assets/services_icons/medEsp/servicio-11.svg'
+import dermaIcon from '@/assets/services_icons/medEsp/servicio-12.svg'
+import reuIcon from '@/assets/services_icons/medEsp/servicio-15.svg'
+import neuroIcon from '@/assets/services_icons/medEsp/servicio-16.svg'
+import nutriIcon from '@/assets/services_icons/medEsp/servicio-17.svg'
+import oftaIcon from '@/assets/services_icons/medEsp/servicio-18.svg'
+import psiIcon from '@/assets/services_icons/medEsp/servicio-20.svg'
+
+
+
+export const servicesMdEsp = [
+    {
+        title: 'Medicina Familiar',
+        content: 'Es una especialidad médica que proporciona cuidado y atención sanitaria continua e integral a las personas y familias.',
+        icon: medFaIcon.src
+    },
+    {
+        title: 'Medicina Interna',
+        content: 'Es una especialidad médica que permite el diagnóstico y el tratamiento integral de los pacientes adultos.',
+        icon: medInIcon.src
+    },
+    {
+        title: 'Pediatria',
+        content: 'La Pediatría se dedica al cuidado integral de la salud de los niños, desde el nacimiento hasta la adolescencia.',
+        icon: pediatriaIcon.src
+    },
+    {
+        title: 'Cardiología',
+        content: 'La Cardiología se encarga del estudio, diagnóstico y tratamiento de las enfermedades del corazón y del sistema circulatorio.',
+        icon: cardiologiaIcon.src
+    },
+    {
+        title: 'Cardiología Pediatrica',
+        content: 'La Cardiología pediátrica se especializa en el diagnóstico y tratamiento de problemas cardíacos en niños.',
+        icon: cardiologiaIcon.src
+    },
+    {
+        title: 'Ginecobstetricia',
+        content: 'La Ginecobstetricia se especializa en la salud femenina, abarcando el embarazo, parto y cuidados postnatales.',
+        icon: ginecobsIcon.src
+    },
+    {
+        title: 'Endocrinología',
+        content: 'Es el campo de la medicina que se relaciona con los sistemas que controlan las hormonas.',
+        icon: endoIcon.src
+    },
+    {
+        title: 'Endocrinología Pediátrica',
+        content: 'Se dedica a evaluar y tratar a pacientes pediátricos con enfermedades endocrinas y desequilibrios hormonales.',
+        icon: endoIcon.src
+    },
+    {
+        title: 'Otorrinolaringología',
+        content: 'La Otorrinolaringología se especializa en el diagnóstico y tratamiento de enfermedades del oído, nariz y garganta.',
+        icon: otoIcon.src
+    },
+    {
+        title: 'Neumología',
+        content: 'Es la disciplina de la medicina que estudia las enfermedades que afectan el aparato respiratorio.',
+        icon: neuIcon.src
+    },
+    {
+        title: 'Neumología Pediátrica',
+        content: 'La Neumología pediátrica se dedica al diagnóstico y tratamiento de enfermedades respiratorias en niños.',
+        icon: neuIcon.src
+    },
+    {
+        title: 'Dermatología',
+        content: 'Es la disciplina de la medicina que se encarga de estudiar la estructura y función de la piel.',
+        icon: dermaIcon.src
+    },
+    {
+        title: 'Nefrología',
+        content: 'La Nefrología se encarga del estudio, diagnóstico y tratamiento de enfermedades del riñón y del sistema urinario.',
+        icon: nefroIcon.src
+    },
+    {
+        title: 'Nefrología Pediatrica',
+        content: 'La Nefrología pediátrica se especializa en el diagnóstico y tratamiento de enfermedades renales en niños.',
+        icon: nefroIcon.src
+    },
+    {
+        title: 'Reumatología',
+        content: 'La Reumatología trata enfermedades de articulaciones, músculos, huesos y tejidos conectivos para mejorar la calidad de vida del paciente.',
+        icon: reuIcon.src
+    },
+    {
+        title: 'Neurología',
+        content: 'Diagnostica y trata enfermedades del sistema nervioso central y periférico, incluyendo cerebro, médula espinal, nervios y músculos.',
+        icon: neuroIcon.src
+    },
+    {
+        title: 'Nutrición y Dieta',
+        content: 'Nuestro servicio de Nutrición ofrece atención personalizada, proporcionando orientación y análisis para desarrollar un plan de acción que mejore su salud.',
+        icon: nutriIcon.src
+    },
+    {
+        title: 'Oftalmología',
+        content: 'Trata enfermedades de los ojos y tejidos cercanos, colaborando con Optometría para manejar defectos visuales y condiciones médico-quirúrgicas.',
+        icon: oftaIcon.src
+    },
+    {
+        title: 'Infectología Pediátrica',
+        content: 'Se especializa en el manejo de enfermedades pediátricas causadas por microorganismos.',
+        icon: neuroIcon.src
+    },
+    {
+        title: 'Psicología',
+        content: 'Nuestro servicio de psicología proporciona atención psicológica adaptada a las necesidades individuales de cada paciente.',
+        icon: psiIcon.src
+    }
+]
