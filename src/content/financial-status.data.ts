a0f5dab357236a40eb057f2f03df76de5429f646
Gabriel Duarte <duartearias2002@gmail.com>
2025-05-05 16:44:53 -0500

    feat: mejorar nombres de documentos en el estado financiero y estilos

--- src/content/financial-status.data.ts
+++ src/content/financial-status.data.ts
@@ -42,39 +42,39 @@ export const financialStatusData = [
     year: 2024,
     documents: [
       {
-        name: "DICTAMEN REVISOR FISCAL NORDVITAL IPS SAS 2024",
+        name: "Dictamen revisor fiscal nordvital ips sas 2024",
         file: dictamenRevisorFiscal2024,
       },
       {
-        name: "NOTAS DE REVELACION NORDVITAL IPS SAS 2024",
+        name: "Notas de revelacion nordvital ips sas 2024",
         file: notasRevelacion2024,
       },
       {
-        name: "CERTIFICADO DE LOS ESTADOS FINANCIEROS 2024",
+        name: "Certificado de los estados financieros 2024",
         file: certificadoEstados2024,
       },
       {
-        name: "ESTADO DE SITUACION FINANCIERA NORDVITAL IPS SAS 2024",
+        name: "Estado de situacion financiera nordvital ips sas 2024",
         file: estadoSituacion2024,
       },
       {
-        name: "ESTADO DE GANANCIAS Y PERDIDAS NORDVITAL IPS SAS 2024",
+        name: "Estado de ganancias y perdidas nordvital ips sas 2024",
         file: estadoGanancias2024,
       },
       {
-        name: "ESTADO FLUJO DE EFECTIVO NORDVITAL IPS 2024",
+        name: "Estado flujo de efectivo nordvital ips 2024",
         file: estadoFlujo2024,
       },
       {
-        name: "7-ESTADO CAMBIOS EN EL PATRIMONIO NORDVITAL IPS 2024",
+        name: "Estado cambios en el patrimonio nordvital ips 2024",
         file: estadoCambios2024,
       },
       {
-        name: "8-DISTRIBUCION DE UTILIDADES 2024",
+        name: "Distribucion de utilidades 2024",
         file: distribucionUtilidades2024,
       },
       {
-        name: "9-INFORME DE GESTION 2024",
+        name: "Informe de gestion 2024",
         file: informeGestion2024,
       },
     ],
@@ -108,7 +108,7 @@ export const financialStatusData = [
         file: estadoSituacion2022,
       },
       {
-        name: "Notas a los Estados de situación financiera.",
+        name: "Notas a los estados de situación financiera.",
         file: notasEstados2022,
       },
       {
