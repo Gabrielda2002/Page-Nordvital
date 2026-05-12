0c6b85b0f81694a5a1c548649fed2fef9b92258c
Gabriel Duarte <duartearias2002@gmail.com>
2026-05-12 17:02:19 -0500

    refactor: desing components

---
+++ src/content/about-us.ts
@@ -0,0 +1,26 @@
+export const PRINCIPLES = [
+    {
+        title: "Amabilidad",
+        desc: "Crear un ambiente acogedor donde cada persona se sienta valorada",
+    },
+    {
+        title: "Trabajo en equipo",
+        desc: "Colaboración, comunicación abierta y apoyo mutuo",
+    },
+    {
+        title: "Liderazgo",
+        desc: "Inspirar a otros a alcanzar su máximo potencial",
+    },
+    {
+        title: "Confianza",
+        desc: "Actuar con integridad, transparencia y consistencia",
+    },
+    {
+        title: "Responsabilidad",
+        desc: "Cumplir obligaciones con diligencia y ética",
+    },
+    {
+        title: "Solidaridad",
+        desc: "Compromiso activo con el bienestar de los demás",
+    },
+]
